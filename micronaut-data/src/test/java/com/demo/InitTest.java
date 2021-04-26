a178fa8993baa2530be8d51467dca3bce72fc941
pankaj khedekar <pankajkhedekar@pankajs-MacBook-Pro.local>
2021-04-26 18:50:25 +0530

    initial commit

---
+++ micronaut-data/src/test/java/com/demo/InitTest.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class InitTest {
+
+    @Test
+    public void dummyTest(){
+        Assertions.assertEquals(Integer.parseInt("1"), 1);
+    }
+}
